f37b1073c9c11c3b72675b14e0a73eea8bf8555d
NiñoDerek <cabaitanneo@gmail.com>
2023-01-14 22:14:54 +0800

    fixed
    fixed

--- app.py
+++ app.py
@@ -2,12 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
-@app.route('/home')
-@app.route('/index')
-@app.route('/base')
 @app.route('/')
-def base():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
 
 @app.route('/login')
 def login():
