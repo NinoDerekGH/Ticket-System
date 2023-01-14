85649c09f7bbb86bf0c3912e3cb5787b46e10747
NiñoDerek <cabaitanneo@gmail.com>
2023-01-14 23:39:54 +0800

    fixed
    panel fixed

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('forms/user.html')
 
 @app.route('/login')
 def login():
