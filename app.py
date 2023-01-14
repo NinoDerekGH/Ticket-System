23fce72f6307537440312c02102fc04c163894ac
NekoMeowCat <107986856+NekoMeowCat@users.noreply.github.com>
2023-01-15 01:32:04 +0800

    1:31AM

--- app.py
+++ app.py
@@ -2,13 +2,21 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
+
 @app.route('/')
 def index():
     return render_template('forms/user.html')
 
+
 @app.route('/login')
 def login():
     return render_template('login.html')
+
+
+@app.route('/unassigned')
+def unassigned():
+    return render_template('vendor/unassigned.html')
+
 
 @app.route('/register')
 def register():
@@ -16,7 +24,7 @@ def register():
 
 # @app.route('/user/create', methods=['POST'])
 # def create():
-    
+
 
 if __name__ == '__main__':
     app.run(debug=True)
