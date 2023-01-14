dfff1d6ef9ac0e37fcd95857234456aa8afcac6a
ArvenCo <arveneco100@gmail.com>
2023-01-14 19:25:11 +0800

    Compose Layout

--- app.py
+++ app.py
@@ -1,6 +1,16 @@
 from flask import Flask, render_template
+from flaskext.mysql import MySQL
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+
+mysql = MySQL()
+
+app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
+app.config['MYSQL_DATABASE_DB'] = 'demo'
+app.config['MYSQL_DATABASE_HOST'] = 'localhost'
+
+mysql.init_app(app)
 
 @app.route('/')
 def index():
