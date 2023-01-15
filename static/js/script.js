0df317c15125b87c888a2cf61d641745e0e929fb
NiñoDerek <cabaitanneo@gmail.com>
2023-01-15 20:39:15 +0800

    register form
    added reg form, ticket header, script for reg form

--- static/js/script.js
+++ static/js/script.js
@@ -1,12 +1,23 @@
 function validateForm() {
+    var firstname = document.getElementById("fname").value;
+    var firstnameError = document.getElementById("fname-error");
+    var lastname = document.getElementById("lname").value;
+    var lastnameError = document.getElementById("lname-error");
     var email = document.getElementById("email").value;
     var emailError = document.getElementById("email-error");
     var password = document.getElementById("pwd").value;
     var passwordError = document.getElementById("password-error");
+
+    if (firstname === "") {
+      firstnameError.insertAdjacentHTML("beforeend", "Field is required!");
+    }
+    if (lastname === "") {
+      lastnameError.insertAdjacentHTML("beforeend", "Field is required!");
+    }
     if (email === "") {
-      emailError.insertAdjacentHTML("beforeend", "Email field is required!");
+      emailError.insertAdjacentHTML("beforeend", "Field is required!");
     }
     if (password === "") {
-      passwordError.insertAdjacentHTML("beforeend", "Password field is required!");
+      passwordError.insertAdjacentHTML("beforeend", "Field is required!");
     }
   }
