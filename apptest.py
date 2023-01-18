2bd7a699a02f4c6bcd815e40396cf54a0d2c5d3c
ArvenCo <arveneco100@gmail.com>
2023-01-18 14:39:41 +0800

    flask sqlalchemy working

---
+++ apptest.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:@localhost:3306/ticketing_db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+
+@app.route('/user', methods=['POST'])
+def create_user():
+   
+    user = User(username='junzel gwapo', email='test@example.com')
+    db.session.add(user)
+    db.session.commit()
+
+with app.app_context():
+    db.create_all()
+    create_user()
+
+    user = User.query.all()
+    print(user)
