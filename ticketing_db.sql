2421a43272b92c351e332aff8e37dbd3cbc8ed76
NekoMeowCat <107986856+NekoMeowCat@users.noreply.github.com>
2023-01-19 13:37:58 +0800

    updated sql data
    sql data

--- ticketing_db.sql
+++ ticketing_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jan 17, 2023 at 09:04 AM
+-- Generation Time: Jan 19, 2023 at 06:36 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -24,27 +24,106 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `accounts`
+--
+
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL,
+  `firstname` varchar(225) DEFAULT NULL,
+  `lastname` varchar(255) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(225) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `files`
+--
+
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL,
+  `data` longblob NOT NULL,
+  `filename` varchar(60) NOT NULL,
+  `ticket_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tickets`
 --
 
 CREATE TABLE `tickets` (
   `id` int(11) NOT NULL,
-  `subject` varchar(250) NOT NULL,
+  `subject` varchar(60) NOT NULL,
   `content` varchar(10000) NOT NULL,
-  `department_id` int(11) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `user_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `update_at` timestamp NULL DEFAULT NULL,
+  `priority` varchar(60) NOT NULL,
+  `agents_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `tickets`
 --
 
-INSERT INTO `tickets` (`id`, `subject`, `content`, `department_id`, `created_at`) VALUES
-(1, 'Sample Ticket Subject', 'I am concerned about the negative impacts of climate change on global food production and security. The effects of rising temperatures and extreme weather events can lead to crop failures and food shortages. This will disproportionately affect low-income communities and those in developing countries. Climate change also exacerbates existing issues like poverty, hunger, and water scarcity. We need to take immediate action to address this issue, such as by investing in sustainable agriculture, reducing greenhouse gas emissions and promoting resilient food systems. This can be done through policies such as carbon pricing, renewable energy development and conservation of natural resources. Additionally, it is important to support small farmers and rural communities, who are often most vulnerable to the effects of climate change. This will help ensure that food systems are adaptable and resilient to the impacts of a changing climate. Failure to take action now will have serious consequences for people and the planet in the long run.', 2, '2023-01-17 07:54:30');
+INSERT INTO `tickets` (`id`, `subject`, `content`, `user_id`, `created_at`, `update_at`, `priority`, `agents_id`) VALUES
+(1, 'dwa', 'dwa', 2, '2023-01-15 10:27:44', NULL, 'normal', NULL),
+(2, 'Subject2', 'Component item', 2, '2023-01-15 15:23:56', NULL, 'normal', NULL),
+(3, 'Sample Questions ', 'dawda', 2, '2023-01-15 16:40:52', NULL, 'normal', NULL),
+(4, 'SAmple sd2', 'SAmple2 SAmpl2', 2, '2023-01-16 05:18:54', NULL, 'normal', NULL),
+(5, 'Absent', 'absent ko karun kay wako ni skwela', 2, '2023-01-16 06:26:09', NULL, 'normal', NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `username` varchar(80) NOT NULL,
+  `hashed_password` varchar(120) NOT NULL,
+  `role` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `hashed_password`, `role`) VALUES
+(1, 'admin', '$2a$12$HrUvSAbOH1y/57XOJJ6bde8PjriKLeel4ktl304FZO0Bt2rQ9yaC6', 'admin'),
+(7, '123', '$2b$12$Cl9PKmLwBUw1YVpiQGsTZukNspoQWkDNyRPYHtUd5jYzCT.H7EWPS', 'user'),
+(8, 'bobo', '$2b$12$vwTpVSkqpYV1oVSNwrgCSeegoiYfMAQ2RNnf8sLWFGXR3msHRU1Ou', 'admin');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `firstname` varchar(255) NOT NULL,
+  `lastname` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `accounts`
+--
+ALTER TABLE `accounts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `files`
+--
+ALTER TABLE `files`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `tickets`
@@ -53,14 +132,39 @@ ALTER TABLE `tickets`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `accounts`
+--
+ALTER TABLE `accounts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `files`
+--
+ALTER TABLE `files`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `tickets`
 --
 ALTER TABLE `tickets`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
